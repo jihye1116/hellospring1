9bb184e15007385c320933469710f12e6352568b
Wickhye <jihyesim170@gmail.com>
2023-04-11 13:37:41 +0900

    Initial commit

---
+++ src/test/java/com/choi/bssm/hellospring1/Hellospring1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.choi.bssm.hellospring1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hellospring1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
